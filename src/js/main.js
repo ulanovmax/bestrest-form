f7bd5f0e8d4189ef4d28896e036f9663c112fce9
ulanovmax <ulanovmaks29@gmail.com>
2023-07-14 14:59:49 +0300

    add additional validation

--- src/js/main.js
+++ src/js/main.js
@@ -15,43 +15,81 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
-});
 
-/* Select functionality */
-const select = document.querySelector('select');
-const choiceLabel = document.getElementById('choice-label');
-const choice = new Choices(select, {
-    itemSelectText: '',
-    searchEnabled: false,
-});
+    /* Select functionality */
+    const select = document.querySelector('select');
+    const choiceLabel = document.getElementById('choice-label');
+    const choice = new Choices(select, {
+        itemSelectText: '',
+        searchEnabled: false,
+    });
 
-select.addEventListener('showDropdown', () => {
     const choiceHeader = choice.containerInner.element;
-    choice.containerOuter.element.parentElement.classList.add('focused');
+    const choiceContainer = choice.containerOuter.element;
 
-    if (choice.getValue()) {
-        choiceHeader.classList.add('default');
-    } else {
-        choiceHeader.classList.remove('default');
-    }
-});
+    select.addEventListener('showDropdown', () => {
+        choice.containerOuter.element.parentElement.classList.add('focused');
 
-select.addEventListener('hideDropdown', () => {
-    choice.containerOuter.element.parentElement.classList.remove('focused');
-    console.log(choice);
-    if (choice.getValue()) choiceLabel.style.display = 'none';
-});
+        if (choice.getValue()) {
+            choiceHeader.classList.add('default');
+        } else {
+            choiceHeader.classList.remove('default');
+        }
+    });
 
-choice.removeActiveItems();
+    select.addEventListener('hideDropdown', () => {
+        choice.containerOuter.element.parentElement.classList.remove('focused');
+        if (choice.getValue()) choiceLabel.style.display = 'none';
+    });
 
-/* Form validation */
+    choice.removeActiveItems();
 
-$('form').validate({
-    rules: {
-        name: {
-            required: true,
-            maxlength: 16,
+    /* Form validation */
+    const mainForm = document.querySelector('form');
+    const submitButton = mainForm.querySelector('button[type=submit]');
+
+    $(mainForm).validate({
+        rules: {
+            name: {
+                required: true,
+                maxlength: 16,
+            },
+
+            surname: {
+                required: true,
+                maxlength: 16,
+            },
+
+            phone: {
+                required: true,
+                phoneUS: true,
+                digits: true,
+            },
+
+            password: {
+                minlength: 8,
+                required: true,
+            },
+
+            confirm: {
+                required: true,
+                equalTo: '#password',
+            },
         },
-    },
-    messages: {},
+    });
+
+    /* Validate select */
+    submitButton.addEventListener('click', () => {
+        const errorLabel = document.createElement('span');
+        errorLabel.classList.add('error-choice');
+        errorLabel.innerHTML = 'This field is required.';
+
+        if (!choice.getValue()) {
+            choiceHeader.classList.add('error');
+            choiceContainer.after(errorLabel);
+        } else {
+            choiceHeader.classList.remove('error');
+            choiceContainer.nextElementSibling.style.display = 'none';
+        }
+    });
 });
